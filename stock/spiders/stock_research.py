7a509b1a1aa873359c4a6275801e74cac55405b7
bash.py <bash.py@gmail.com>
2015-03-22 21:47:45 +0530

    None

--- stock/spiders/stock_research.py
+++ stock/spiders/stock_research.py
@@ -6,3 +6,7 @@ class stock_r(BaseSpider):
     name = "stock_r"
     allowed_domains = ["http://www.topstockresearch.com"]
     start_urls = ["http://www.topstockresearch.com/PriceVolumeSurprisers/TopGainersIndianStockMarket.html"]
+    def parse(self, response):
+        sel = Selector(response)
+        sites = sel.xpath('//table')
+        items = []
