6d5f1b5978b022cc9e7212e47af37a04670fc234
bashpy <bash.py@gmail.com>
2015-03-23 13:47:42 +0530

    added lists

--- stock/spiders/stock_research.py
+++ stock/spiders/stock_research.py
@@ -1,12 +1,21 @@
 __author__ = 'anom'
 from scrapy.spider import BaseSpider
-from scrapy.selector import Selector
+from scrapy.selector import HtmlXPathSelector
 
 class stock_r(BaseSpider):
     name = "stock_r"
     allowed_domains = ["http://www.topstockresearch.com"]
     start_urls = ["http://www.topstockresearch.com/PriceVolumeSurprisers/TopGainersIndianStockMarket.html"]
     def parse(self, response):
-        sel = Selector(response)
-        sites = sel.xpath('//table')
+        sel = HtmlXPathSelector(response)
+        sites = sel.xpath('//tr/td/text()').extract()
         items = []
+        for item in sites:
+            item = item.encode("utf-8")
+            item = item.strip(" ")
+            item = item.strip('\r\n')
+            item = item.strip('\xc2\xa0')
+            items.append(item)
+            items = filter(None,items)
+        items_list = [items[x:x+7] for x in range(0, len(items),7)]
+        print items_list[49]
